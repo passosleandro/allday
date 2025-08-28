e377ea68128067d8452aeb055411d64b8612fc4b
LEANDRO JOSE FERREIRA PASSOS <ferreiraepassos@hotmail.com>
2025-08-28 16:15:10 -0300

    Add files via upload

---
+++ cript.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+    addToCartButtons.forEach(button => {
+        button.addEventListener('click', () => {
+            alert('Produto adicionado ao carrinho!');
+        });
+    });
+});
